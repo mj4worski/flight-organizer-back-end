4d293a7782ac817af626b5b6aa98f26a52cae85e
Mateusz <jaworskimateusz2@gmail.com>
2017-06-29 18:52:24 +0200

    1. Configure authentication by login REST.

--- src/main/java/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/infrastructure/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +13,14 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final SpringDataJpaUserDetailsService userDetailsService;
 
-    public SecurityConfiguration(SpringDataJpaUserDetailsService userDetailsService) {
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    public SecurityConfiguration(SpringDataJpaUserDetailsService userDetailsService, AuthenticationSuccessHandler authenticationSuccessHandler, RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
         this.userDetailsService = userDetailsService;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
     }
 
     @Override
@@ -25,22 +32,19 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
                 .authorizeRequests()
-                .antMatchers( "/public/**","/console/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/**").authenticated()
+                .antMatchers( "/console/**", "/login").permitAll()
                 .and()
-                .csrf().disable();
-        http
-                .headers()
-                .frameOptions()
-                .sameOrigin();
-        http
                 .formLogin()
-                .loginProcessingUrl("/login").permitAll()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
                 .and()
-                .logout().permitAll()
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+                .logout();
     }
 
 }
